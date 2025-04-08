5a6f6c6c81369549fd46b8211e543d7194ea019d
DairCabrera <daircabrera19@gmail.com>
2025-04-07 19:09:56 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to common banking app. What would you like to do?")
+
+menu = int(input("(1)Login:\n(2)Sign up:\n-"))
+
+if menu == 1:
+    print("Cool")
+if menu == 2:
+    print("Coolaroo!")
