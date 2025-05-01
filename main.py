b5d5764cad60a97961c81aa511e07e8a099f0d69
DairCabrera <daircabrera19@gmail.com>
2025-05-01 17:58:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import mysql.connector
-import time
-import os
-import keyboard
 
-connection = mysql.connector.connect(user = 'root', database = 'Bank', password = 'Davetali05')
+connection = mysql.connector.connect(host = "localhost", user = 'root', database = 'Bank', password = 'Davetali05')
 connection.close()
 
-print("Welcome to common banking app. What would you like to do?\noption1:<\noption1:\noption1:\noption1:")
+("temp")
