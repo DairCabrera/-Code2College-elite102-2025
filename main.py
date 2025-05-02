2afeab7118f4fc4b8490e32a7ea80d14b388f0fd
DairCabrera <daircabrera19@gmail.com>
2025-05-01 21:42:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 import mysql.connector
 
-connection = mysql.connector.connect(host = "localhost", user = 'root', database = 'Bank', password = 'Davetali05')
-connection.close()
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database="Bank",
+    password="Davetali05"
+)
 
-("temp")
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM bank.`bank details`;"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+# print(myresult)
+
+
+def main_menu():
+    test_menu = int(input("what would you like to do?(1 withdraw, 2 deposite, 3 modify acount, 4 delete acount."))
+    if test_menu == 1:
+        print("choice")
+    elif test_menu == 2:
+        print("choice")
+    elif test_menu == 3:
+        print("choice")
+    elif test_menu == 4:
+        print("choice")
+    else:
+        print("invalid")
+        main_menu()
+
+
+def signin():
+    User = input("put your name here: ")
+    sql_select_user = "SELECT * FROM bank.`bank details` WHERE User = %s"
+    mycursor.execute(sql_select_user, (User,))
+    user_data = mycursor.fetchone()
+
+    if user_data:
+        pin_input = input("insert pin:")
+        sql_select_pin = "SELECT Pin FROM bank.`bank details` WHERE User = %s AND Pin = %s"
+        mycursor.execute(sql_select_pin, (User, pin_input))
+        pin_data = mycursor.fetchone()
+        if pin_data:
+            main_menu()
+            return
+        else:
+            print("Incorrect PIN.")
+            return  # Exit the function after incorrect PIN
+    else:
+        print("Wrong name entered.")
+        signin()
+
+signin()
+
+def withdraw
